243decca9c80176e2ad76c6fcbe347539a1709b9
kumarbaberwal <niteshkumarghn1@gmail.com>
2025-03-08 13:54:38 +0530

    webhook - createUser.ts

---
+++ convex/users.ts
@@ -0,0 +1,33 @@
+import { mutation } from "./_generated/server";
+import { v } from "convex/values";
+
+// Create a new task with the given text
+export const createUser = mutation({
+    args: {
+        username: v.string(),
+        fullname: v.string(),
+        email: v.string(),
+        bio: v.optional(v.string()),
+        image: v.string(),
+        clerkId: v.string(),
+    },
+    handler: async (context, args) => {
+        const existingUser = await context.db.query('users').withIndex('by_clerk_id', (index) =>
+            index.eq('clerkId', args.clerkId)
+        ).first()
+
+        if (existingUser) return
+
+        await context.db.insert('users', {
+            username: args.username,
+            fullname: args.fullname,
+            email: args.email,
+            bio: args.bio,
+            image: args.image,
+            clerkId: args.clerkId,
+            followers: 0,
+            following: 0,
+            posts: 0,
+        });
+    }
+});
