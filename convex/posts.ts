98fc57fc3c9ee6c6a5ef6502827fb223e95169e9
kumarbaberwal <niteshkumarghn1@gmail.com>
2025-03-14 12:16:17 +0530

    deletePost - added

--- convex/posts.ts
+++ convex/posts.ts
@@ -1,6 +1,6 @@
 import { v } from "convex/values";
 import { mutation, query } from "./_generated/server";
-import { getAuthenticatedUser } from "./users";
+import { createUser, getAuthenticatedUser } from "./users";
 
 
 export const generateUploadUrl = mutation(async (ctx, args) => {
@@ -120,5 +120,57 @@ export const toggleLike = mutation({
 
             return true;
         }
+    }
+})
+
+
+export const deletePost = mutation({
+    args: {
+        postId: v.id('post')
+    },
+    handler: async (ctx, args) => {
+        const currentUser = await getAuthenticatedUser(ctx)
+
+        const post = await ctx.db.get(args.postId)
+        if (!post) throw new Error("Post not found")
+
+        // veryfy ownership
+
+        if (post.userId !== currentUser._id) throw new Error('Not authorized to delete this post');
+
+
+        // delete associated likes
+        const likes = await ctx.db.query('likes').withIndex('by_post_id', (q) => q.eq('postId', args.postId)).collect()
+
+        for (const like of likes) {
+            await ctx.db.delete(like._id);
+        }
+
+        // delete associated comments 
+        const comments = await ctx.db.query('comments').withIndex('by_post_id', (q) => q.eq('postId', args.postId)).collect();
+
+        for (const comment of comments) {
+            await ctx.db.delete(comment._id);
+        }
+
+        // delete associated bookmarks
+
+        const bookmarks = await ctx.db.query('bookmark').withIndex('by_post_id', (q) => q.eq('postId', args.postId)).collect();
+
+        for (const bookmark of bookmarks) {
+            await ctx.db.delete(bookmark._id)
+        }
+
+        // delete the post from storage
+        await ctx.storage.delete(post.storageId);
+
+        // delete post
+        await ctx.db.delete(args.postId)
+
+        // decreament user's post count by 1
+
+        await ctx.db.patch(currentUser._id, {
+            posts: Math.max(0, (currentUser.posts || 1) - 1)
+        })
     }
 })
